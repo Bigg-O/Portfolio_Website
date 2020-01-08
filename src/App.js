df2f431db05ac4bdb59dc49f34d731bf95913996
Wookeun Song <wookeun.s@gmail.com>
2020-01-07 21:58:39 -0600

    NavBar Buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Homebar from './Components/Homebar'
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Homebar />
-    </div>
+    <Router>
+      <div className="App">
+        <Homebar />
+        <Route exact path="/">
+          
+        </Route>
+      </div>
+    </Router>
   );
 }
 
