6ce43f74a1b4f20d9951834496e9b0c024550b74
Wookeun Song <wookeun.s@gmail.com>
2020-01-07 20:56:55 -0600

    fixed infinite loop

--- src/Components/Homebar.jsx
+++ src/Components/Homebar.jsx
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import Nav from 'react-bootstrap/Nav'
 import Image from 'react-bootstrap/Image'
 import logo from '../Images/Logo.png'
+import Navbar from 'react-bootstrap/Navbar'
+import { Link } from "react-router-dom";
 
-export class Navbar extends Component {
+export class Homebar extends Component {
   render() {
     return (
       <Navbar variant="dark" fixed="top">
@@ -11,13 +13,13 @@ export class Navbar extends Component {
           <Image className="logo" src={logo} fluid />
         </Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#Resumes">Resumes</Nav.Link>
-          <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="#Contact">Contact</Nav.Link>
+          <Nav.Link href="resumes">Resumes</Nav.Link>
+          <Nav.Link href="portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="contact">Contact</Nav.Link>
         </Nav>
       </Navbar>
     );
   }
 }
 
-export default Navbar;
+export default Homebar;
