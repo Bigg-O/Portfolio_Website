03dc29c7256d34b979723237d029be3fe4a7f341
Wookeun Song <wookeun.s@gmail.com>
2020-01-15 14:19:18 -0600

    navbar done

--- src/Components/Homebar.jsx
+++ src/Components/Homebar.jsx
@@ -4,49 +4,60 @@ import logo from "../Images/Name_Logo.png";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink, Link } from "react-router-dom";
+import "./css/Homebar.css";
 
 import Image from "react-bootstrap/Image";
 
 export class Homebar extends Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" variant="dark" fixed="top">
-        <Link to="/">
-          <Navbar.Brand>
-            <Image className="name_logo" src={logo} fluid />
-          </Navbar.Brand>
-        </Link>
+      <Navbar collapseOnSelect expand="md" variant="dark" fixed="top">
+        <Navbar.Brand className="brand" as={NavLink} to="/">
+          <Image src={logo} fluid />
+        </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="links">
-            <Link to="/About">About Me</Link>
+          <div className="mr-auto"></div>
+          <Nav className="nav">
+            <Nav.Link className="text-white" as={NavLink} to="/About">
+              About Me
+            </Nav.Link>
 
             <NavDropdown title="Portfolio">
-              <NavDropdown.Item as={NavLink} to={"/Software_Portfolio"}>
+              <NavDropdown.Item as={NavLink} to="/Software_Portfolio">
                 Software Engineer
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to={"/Robotics_Portfolio"}>
+              <NavDropdown.Item as={NavLink} to="/Robotics_Portfolio">
                 Robotics
               </NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="Resume">
-              <NavDropdown.Item as={NavLink} to={"/Software_Resume"}>
+              <NavDropdown.Item as={NavLink} to="/Software_Resume">
                 Sofeware Engineer
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to={"/Routesetting_Resume"}>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={NavLink} to="/Routesetting_Resume">
                 Routesetting
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to={"/Climbing_Resume"}>
+              <NavDropdown.Item as={NavLink} to="/Climbing_Resume">
                 Rock Climbing
               </NavDropdown.Item>
             </NavDropdown>
 
-            <a href="https://github.com/Bigg-O">GitHub</a>
-            <a href="https://www.linkedin.com/in/wookeun-s/">LinkedIn</a>
-            <Link to="/Contact">Contact</Link>
+            <Nav.Link className="link" href="https://github.com/Bigg-O">
+              GitHub
+            </Nav.Link>
+            <Nav.Link
+              className="link"
+              href="https://www.linkedin.com/in/wookeun-s/"
+            >
+              LinkedIn
+            </Nav.Link>
+            <Nav.Link className="text-white" as={NavLink} to="/Contact">
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
