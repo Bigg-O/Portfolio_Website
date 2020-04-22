c3ac4eaa4d9023cbf4b824d2ee9dee7158885670
Wookeun Song <wookeun.s@gmail.com>
2020-04-21 20:00:42 -0500

    about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -20,7 +20,7 @@ export class About extends Component {
           </Col>
           <Col as={Card} border="dark" bg="light" sm={7}>
             <h4>
-              Hi I'm Wookeun Song!{" "}
+              Hi I'm Wookeun Oscar Song!{" "}
               <span role="img" aria-label="flags">
                 🇲🇽 🇰🇷 🇺🇸
               </span>
@@ -32,29 +32,28 @@ export class About extends Component {
               <br />
             </p>
             <p className="aboutMe">
-              I am a Full Stack Web Developer with experience in both SQL and
+              I am a software developer with robotics software engineering and full stack web development experiences. I've used both SQL and
               NoSQL databases using Ruby on Rails and MERN (Mongo, Express,
               React, and Node) Stack frameworks. I’ve been a passionate
-              programmer since 2013 when I joined a FIRST Robotics Competition
-              team. As a lead programmer of the team, I learned to code with
-              other programmers and communicate with other types of engineers,
+              developer since 2013 when I joined a FIRST Robotics Competition
+              team. As a lead robotics software/electrical engineer of the team, I learned to code with
+              other software engineers and communicate with other types of engineers,
               giving me considerable experience with solving real life problems
               with code. Due to the programming team’s success that I led at
               competitions in making final rounds consecutively at FRC, it led
               me to be a leader at Nao Robot Fashion Show at Barnes & Noble
-              Robotics Fair, giving me an opportunity to lead teams of
+              Robotics Fair, giving me an another opportunity to lead teams of
               engineers. In order to pursue a professional software engineering
-              career, took computer science courses at the University of Texas
-              at Austin, setting me up with a strong data structure and
-              low-level computer science background. After the course of
+              career, took computer science major courses at the University of Texas
+              at Austin, setting me up with a strong fundamental computer science background. After the course of
               learning most up to date, web development frameworks, currently
-              building solo projects for fun!
+              building solo projects as a freelance web developer!
             </p>
             <p className="aboutMe">
               As much as I enjoy the world of technology, I love exploring the
-              real world. I’ve gone rock climbing all over the US, Mexico,
+              real world. I’ve gone rock climbing all over the world including US, Mexico,
               Thailand, and Korea to find the purest and the most breath-taking
-              challenging lines out there. Instead of just ‘being’ outdoors, I’m
+              challenging lines out there. Instead of just ‘being’ outdoors, I like to be
               ‘doing’ the outdoors. My passion in climbing took me on a wild
               rocky journey from working at Austin Bouldering Project as a
               routesetter in the biggest bouldering gym in North America, to all
@@ -63,9 +62,9 @@ export class About extends Component {
             </p>
             <p className="aboutMe">
               One may say it’s an obsession or others may say it’s a passion. Do
-              I mind what it is? Nopp. I just want to keep doing what's fun and
+              I mind what it is? Nope. I just want to keep doing what's fun and
               hopefully be that one person who is going to be known for being
-              best at what I do.
+              the best at what I do.
             </p>
           </Col>
         </Row>
